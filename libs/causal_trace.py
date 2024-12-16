e4e00ece55b17709570f98efcf403333ed50ec47
Yang Han <164020610+yanghangit@users.noreply.github.com>
2024-12-16 17:14:27 +0800

    Add files via upload

---
+++ libs/causal_trace.py
@@ -0,0 +1,128 @@
+# Modified based on https://github.com/kmeng01/rome/ (MIT License)
+
+import torch
+import numpy as np
+
+from collections import defaultdict
+
+from .nethook import TraceDict
+
+def layername(model, num, kind=None):
+    if hasattr(model, "bert"):
+        if kind == "embed":
+            return "bert.embeddings.word_embeddings"
+        return f'bert.encoder.layer.{num}{"" if kind is None else "." + kind}'
+    assert False, "unknown transformer structure"
+
+def trace_with_patch(
+    model,  # The model
+    inp,  # A set of inputs
+    states_to_patch,  # A list of (token index, layername) triples to restore
+    tokens_to_mix,  # Range of tokens to corrupt (begin, end)
+    noise=0.1,  # Level of noise to add
+    uniform_noise=False,
+    replace=False,  # True to replace with instead of add noise
+    trace_layers=None,  # List of traced outputs to return
+):
+    """
+    Runs a single causal trace.  Given a model and a batch input where
+    the batch size is at least two, runs the batch in inference, corrupting
+    a the set of runs [1...n] while also restoring a set of hidden states to
+    the values from an uncorrupted run [0] in the batch.
+
+    The convention used by this function is that the zeroth element of the
+    batch is the uncorrupted run, and the subsequent elements of the batch
+    are the corrupted runs.  The argument tokens_to_mix specifies an
+    be corrupted by adding Gaussian noise to the embedding for the batch
+    inputs other than the first element in the batch.  Alternately,
+    subsequent runs could be corrupted by simply providing different
+    input tokens via the passed input batch.
+
+    Then when running, a specified set of hidden states will be uncorrupted
+    by restoring their values to the same vector that they had in the
+    zeroth uncorrupted run.  This set of hidden states is listed in
+    states_to_patch, by listing [(token_index, layername), ...] pairs.
+    To trace the effect of just a single state, this can be just a single
+    token/layer pair.  To trace the effect of restoring a set of states,
+    any number of token indices and layers can be listed.
+    """
+
+    rs = np.random.RandomState(1)  # For reproducibility, use pseudorandom noise
+    if uniform_noise:
+        prng = lambda *shape: rs.uniform(-1, 1, shape)
+    else:
+        prng = lambda *shape: rs.randn(*shape)
+
+    patch_spec = defaultdict(list)
+    for t, l in states_to_patch:
+        patch_spec[l].append(t)
+
+    embed_layername = "bert.embeddings.word_embeddings"
+
+    def untuple(x):
+        return x[0] if isinstance(x, tuple) else x
+
+    # Define the model-patching rule.
+    if isinstance(noise, float):
+        noise_fn = lambda x: noise * x
+    else:
+        noise_fn = noise
+
+    def patch_rep(x, layer):
+        if layer == embed_layername:
+            # If requested, we corrupt a range of token embeddings on batch items x[1:]
+            if tokens_to_mix is not None:
+                noise_data = noise_fn(
+                    torch.from_numpy(prng(x.shape[0] - 1, len(tokens_to_mix), x.shape[2]))
+                ).to(x.device)
+                if replace:
+                    x[1:, tokens_to_mix] = noise_data
+                else:
+                    x[1:, tokens_to_mix] += noise_data
+            return x
+        if layer not in patch_spec:
+            return x
+        # If this layer is in the patch_spec, restore the uncorrupted hidden state
+        # for selected tokens.
+        h = untuple(x)
+        for t in patch_spec[layer]:
+            h[1:, t] = h[0, t]
+        return x
+
+    # With the patching rules defined, run the patched model in inference.
+    additional_layers = [] if trace_layers is None else trace_layers
+    with torch.no_grad(), TraceDict(
+        model,
+        [embed_layername] + list(patch_spec.keys()) + additional_layers,
+        edit_output=patch_rep,
+    ) as td:
+        outputs_exp = model(inp)
+
+    # prob of getting AD
+    probs = torch.softmax(outputs_exp.logits[1:, :], dim=1).mean(dim=0)[1]
+
+    # If tracing all layers, collect all activations together to return.
+    if trace_layers is not None:
+        all_traced = torch.stack(
+            [untuple(td[layer].output).detach().cpu() for layer in trace_layers], dim=2
+        )
+        return probs, all_traced
+
+    return probs
+
+def trace_important_states(model, num_layers, inp, e_range, noise=0.1):
+    ntoks = inp.shape[1]
+    table = []
+    for tnum in range(ntoks):
+        row = []
+        for layer in range(0, num_layers):
+            r = trace_with_patch(
+                model,
+                inp,
+                [(tnum, layername(model, layer))],
+                tokens_to_mix=e_range,
+                noise=noise,
+            )
+            row.append(r)
+        table.append(torch.stack(row))
+    return torch.stack(table)
